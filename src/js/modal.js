f8b99779594c6bbfaae03eb1979cb87befdeea5b
Konstantyn Radchenko <radchenko1904@gmail.com>
2021-07-19 18:00:08 +0300

    markup modaljs

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,24 @@
     refs.modal.classList.toggle('backdrop--is-hidden');
   }
 })();
+// ;(() => {
+//   const refs = {
+//     openModalBtns: document.querySelectorAll('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   }
+
+//   refs.openModalBtns.forEach((btn) => {
+//     btn.addEventListener('click', toggleModal)
+//   })
+//   refs.closeModalBtn.addEventListener('click', toggleModal)
+//   window.addEventListener('keydown', (e) => {
+//     if (e.code === 'Escape') {
+//       toggleModal()
+//     }
+//   })
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden')
+//   }
+// })()
