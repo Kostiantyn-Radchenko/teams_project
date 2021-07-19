c062c781da5adf14ebb10c1850027ec90b737f73
Konstantyn Radchenko <radchenko1904@gmail.com>
2021-07-19 20:22:40 +0300

    repair modal buttons

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,35 +1,38 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('backdrop--is-hidden');
-  }
-})();
-// ;(() => {
+// (() => {
 //   const refs = {
-//     openModalBtns: document.querySelectorAll('[data-modal-open]'),
+//     openModalBtn: document.querySelector('[data-modal-open]'),
 //     closeModalBtn: document.querySelector('[data-modal-close]'),
 //     modal: document.querySelector('[data-modal]'),
-//   }
+//   };
 
-//   refs.openModalBtns.forEach((btn) => {
-//     btn.addEventListener('click', toggleModal)
-//   })
-//   refs.closeModalBtn.addEventListener('click', toggleModal)
-//   window.addEventListener('keydown', (e) => {
-//     if (e.code === 'Escape') {
-//       toggleModal()
-//     }
-//   })
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
 //   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden')
+//     refs.modal.classList.toggle('backdrop--is-hidden');
 //   }
-// })()
+// })();
+
+
+//////////////////////////////////////
+;(() => {
+  const refs = {
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  }
+
+  refs.openModalBtns.forEach((btn) => {
+    btn.addEventListener('click', toggleModal)
+  })
+  refs.closeModalBtn.addEventListener('click', toggleModal)
+  window.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      toggleModal()
+    }
+  })
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden')
+  }
+})()
